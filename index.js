73a865b5321c3d39685b851848d1c685640afd72
FelixOmollo <omollof418@gmail.com>
2023-04-01 19:05:00 +0300

    Changed the layout using javascript

--- index.js
+++ index.js
@@ -2,68 +2,89 @@ const context = {
     portfolio:[
         {
             image: 'Chess-Club Logo.jpg',
-            description: 'Udi',
-            strengths: 'Nani was in Taita Taaveta University between 2018 & 2023. It is approximated that he had a rating of 1900.'
+            name: 'Udi',
+            strengths: '',
+            personality: 'Bernard alias berben, tsunami and meli was one of the adent chess players at the institution. He graduated with a Bsc in Analytical Chemistry.'
         },
         {
             image:'Chess-Club Logo.jpg',
-            description:'Nick',
-            strengths: 'Nani was in Taita Taaveta University between 2018 & 2023. It is approximated that he had a rating of 1900.'
+            name:'Nick',
+            strengths: '',
+            personality: 'Bernard alias berben, tsunami and meli was one of the adent chess players at the institution. He graduated with a Bsc in Analytical Chemistry.'
         },
         {
             image:'Chess-Club Logo.jpg',
-            description:'Boni',
-            strengths: 'Nani was in Taita Taaveta University between 2018 & 2023. It is approximated that he had a rating of 1900.'
+            name:'Boni',
+            strengths: '',
+            personality: 'Bernard alias berben, tsunami and meli was one of the adent chess players at the institution. He graduated with a Bsc in Analytical Chemistry.'
         },
         {
             image:'Chess-Club Logo.jpg',
-            description:'Gich',
-            strengths: 'Nani was in Taita Taaveta University between 2018 & 2023. It is approximated that he had a rating of 1900.'
+            name:'Gich',
+            strengths: '',
+            personality: 'Bernard alias berben, tsunami and meli was one of the adent chess players at the institution. He graduated with a Bsc in Analytical Chemistry.'
         },
         {
             image:'Chess-Club Logo.jpg',
-            description:'Bern bern',
-            strengths: 'Nani was in Taita Taaveta University between 2018 & 2023. It is approximated that he had a rating of 1900.'
+            name:'Bern bern',
+            strengths: '',
+            personality: 'Bernard alias berben, tsunami and meli was one of the adent chess players at the institution. He graduated with a Bsc in Analytical Chemistry.'
 
         },
         {
             image:'Chess-Club Logo.jpg',
-            description:'Emily',
-            strengths: 'Nani was in Taita Taaveta University between 2018 & 2023. It is approximated that he had a rating of 1900.'
+            name:'Emily',
+            strengths: '',
+            personality: 'Bernard alias berben, tsunami and meli was one of the adent chess players at the institution. He graduated with a Bsc in Analytical Chemistry.'
 
         },
         {
             image:'Chess-Club Logo.jpg',
-            description:'Clinton',
-            strengths: 'Nani was in Taita Taaveta University between 2018 & 2023. It is approximated that he had a rating of 1900.'
+            name:'Clinton',
+            strengths: '',
+            personality: 'Bernard alias berben, tsunami and meli was one of the adent chess players at the institution. He graduated with a Bsc in Analytical Chemistry.'
 
         },
         {
             image:'Chess-Club Logo.jpg',
-            description:'Danson',
-            strengths: 'Nani was in Taita Taaveta University between 2018 & 2023. It is approximated that he had a rating of 1900.'
+            name:'Danson',
+            strengths: '',
+            personality: 'Bernard alias berben, tsunami and meli was one of the adent chess players at the institution. He graduated with a Bsc in Analytical Chemistry.'
 
         },
         {
             image:'Chess-Club Logo.jpg',
-            description:'Senetor',
-            strengths: 'Nani was in Taita Taaveta University between 2018 & 2023. It is approximated that he had a rating of 1900.'
+            name:'Senetor',
+            strengths: '',
+            personality: 'Bernard alias berben, tsunami and meli was one of the adent chess players at the institution. He graduated with a Bsc in Analytical Chemistry.'
 
         },
         {
             image:'Chess-Club Logo.jpg',
-            description:'YG',
-            strengths: 'Nani was in Taita Taaveta University between 2018 & 2023. It is approximated that he had a rating of 1900.'
+            name:'YG',
+            strengths: '',
+            personality: 'Bernard alias berben, tsunami and meli was one of the adent chess players at the institution. He graduated with a Bsc in Analytical Chemistry.'
 
         },
         {
             image:'Chess-Club Logo.jpg',
-            description:'Kevoh',
-            strengths: 'Nani was in Taita Taaveta University between 2018 & 2023. It is approximated that he had a rating of 1900.'
+            name:'Kevoh',
+            strengths: '',
+            personality: 'Bernard alias berben, tsunami and meli was one of the adent chess players at the institution. He graduated with a Bsc in Analytical Chemistry.'
+
+        },
+        {
+            image:'Chess-Club Logo.jpg',
+            name:'Shamir',
+            strengths: '',
+            personality: 'Bernard alias berben, tsunami and meli was one of the adent chess players at the institution. He graduated with a Bsc in Analytical Chemistry.'
 
         }
     ]
 };
+context.portfolio.forEach(item =>{
+    item.strengths = `${item.personality}`;
+});
 
 const templateElement = document.getElementById('templateHB');
 const templateSource = templateElement.innerHTML;
@@ -72,3 +93,21 @@ const compiledHTML = template(context);
 
 //Rendering the template onto the webpage
 document.getElementById('display-players').innerHTML = compiledHTML;
+
+
+//On mouseover and out
+const hiddenInfo = document.querySelector('.strengths');
+
+const displayPersonality = (event) => {
+    hiddenInfo.style.display = 'block';
+    hiddenInfo.style.width = '200px';
+};
+
+const returnToDefault = (event) => {
+    hiddenInfo.style.display = '';
+    hiddenInfo.style.width = '';
+}
+
+//adding event listener
+hiddenInfo.addEventListener('mouseover', displayPersonality);
+hiddenInfo.addEventListener('mouseout', returnToDefault);
